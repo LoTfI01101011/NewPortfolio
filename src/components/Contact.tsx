91955d3a344a2e7f32e1beeeb087ccdc41891d8a
Lotfi <lotfi.kaddari.lp2@gmail.com>
2024-10-09 18:04:10 +0100

    fixing responsive bug

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,31 +5,36 @@ import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div className='w-full h-[550px] mx-auto flex justify-center items-center pb-4'>
-      <div className='w-[800px] h-full m-4 pb-4 border-8 border-slate-300 rounded-xl bg-slate-900'>
-        <div className="m-4 flex flex-col">
-          <h1 className="text-xl text-slate-50 font-semibold">Contact Me:</h1>
-          <div className="mt-10 flex flex-wrap items-center justify-center gap-7">
+    <div className="w-full min-h-screen flex justify-center items-center pb-4">
+      <div className="w-[90%] max-w-[800px] h-full p-6 border-8 border-slate-300 rounded-xl bg-slate-900 shadow-lg">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl text-slate-50 font-bold mb-6">Contact Me:</h1>
+          
+          <div className="mt-8 flex flex-wrap items-center justify-center gap-8">
             <Link to='https://github.com/LoTfI01101011'>
-              <BsGithub size={60} className="m-3 rounded-full text-slate-50 hover:text-white shadow-md transition duration-300 transform hover:scale-110" />
+              <BsGithub size={60} className="transform hover:scale-110 transition-transform duration-300 text-slate-50 hover:text-white shadow-lg hover:shadow-white" />
             </Link>
             <Link to='https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=lotfi.kaddari.lp2@gmail.com'>
-              <SiGmail size={60} className="m-3 rounded-2xl text-red-500 opacity-80 hover:opacity-100 transition duration-300 transform hover:scale-110" />
+              <SiGmail size={60} className="transform hover:scale-110 transition-transform duration-300 text-red-500 opacity-80 hover:opacity-100" />
             </Link>
             <Link to='https://www.linkedin.com/in/lotfi-tadjeddine-kaddari-060a612a6/'>
-              <BsLinkedin size={60} className="m-3 rounded-2xl text-blue-500 opacity-80 hover:opacity-100 transition duration-300 transform hover:scale-110" />
+              <BsLinkedin size={60} className="transform hover:scale-110 transition-transform duration-300 text-blue-500 opacity-80 hover:opacity-100" />
             </Link>
             <Link to='https://www.instagram.com/lotfi_kadari/'>
-              <BsInstagram size={60} className="m-3 rounded-2xl text-yellow-400 opacity-80 hover:opacity-100 transition duration-300 transform hover:scale-110" />
+              <BsInstagram size={60} className="transform hover:scale-110 transition-transform duration-300 text-yellow-400 opacity-80 hover:opacity-100" />
             </Link>
             <Link to='https://x.com/KaddariL?t=JxdVnelH6rd6g3R-GRvrrw&s=09'>
-              <FaXTwitter size={60} className="m-3 rounded-full text-slate-50 hover:text-white shadow-md transition duration-300 transform hover:scale-110" />
+              <FaXTwitter size={60} className="transform hover:scale-110 transition-transform duration-300 text-slate-50 hover:text-white shadow-lg hover:shadow-white" />
             </Link>
           </div>
-          <h1 className="text-lg text-slate-50 font-semibold mt-16">Phone: +213773238009</h1>
-          <h1 className="text-lg text-slate-50 font-semibold my-8">Mail: lotfi.kaddari.lp2@gmail.com</h1>
-          <Link to='https://drive.google.com/uc?export=download&id=1T6Pj2qY6mxjsSZHSWOJQ_Fy3_FaFsjpC' className="mx-auto mt-10">
-            <div className="rounded-full p-4 border-2 border-slate-50 text-slate-50 font-semibold hover:bg-white hover:text-black transition duration-300">
+
+          <div className="mt-12 w-full text-center">
+            <h1 className="text-lg text-slate-50 font-semibold">Phone: +213773238009</h1>
+            <h1 className="text-lg text-slate-50 font-semibold my-4">Mail: lotfi.kaddari.lp2@gmail.com</h1>
+          </div>
+
+          <Link to='https://drive.google.com/uc?export=download&id=1T6Pj2qY6mxjsSZHSWOJQ_Fy3_FaFsjpC' className="mt-8">
+            <div className="rounded-full p-4 border-2 border-slate-50 text-slate-50 font-semibold hover:bg-white hover:text-black transition-all duration-300 ease-in-out transform hover:scale-105">
               Download CV
             </div>
           </Link>
